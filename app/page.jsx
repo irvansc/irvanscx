10f78af0ac22d5ba865a8f597441673540b94e08
Irvanscx <irvanmaulana292@gmail.com>
2024-09-01 15:45:11 +0700

    first commit

--- app/page.jsx
+++ app/page.jsx
@@ -10,13 +10,13 @@ const Home = () => {
       <div className="container mx-auto h-full">
         <div className="flex flex-col xl:flex-row items-center justify-between xl:pt-8 xl:pb-24">
           <div className="text-center xl:text-left order-2 xl:-order-none">
-            <span className="text-xl ">Software Developer</span>
+            <span className="text-xl ">Web Developer</span>
             <h1 className="h1 mb-6">
               Hello I&apos;m <br />{" "}
               <span className="text-accent">Irvanscx</span>
             </h1>
             <p className="max-w-[500px] mb-9 text-white/80">
-              I am a software developer with a passion for creating innovative
+              I am a Web Developer with a passion for creating innovative
               solutions to complex problems. I am a quick learner and a team
               player.
             </p>
--- components/Stats.jsx
+++ components/Stats.jsx
@@ -3,7 +3,7 @@ import CountUp from "react-countup";
 
 const Stat = [
   {
-    num: 12,
+    num: 4,
     text: "Years Of Experience",
   },
   {
@@ -24,18 +24,18 @@ const Stats = () => {
   return (
     <section className="pt-4 pb-12 xl:pt-0 xl:pb-0">
       <div className="container mx-auto">
-        <div className="flex flex-wrap gap-6 max-w-[80vw] mx-auto xl:max-w-none">
+        <div className="grid grid-cols-2 xl:grid-cols-4 gap-6 max-w-[90vw] sm:max-w-[80vw] mx-auto xl:max-w-none">
           {Stat.map((item, index) => {
             return (
               <div
-                className="flex-1 flex gap-4 items-center justify-center xl:justify-start"
+                className="flex gap-4 items-center justify-center xl:justify-start"
                 key={index}
               >
                 <CountUp
                   end={item.num}
                   duration={5}
                   delay={2}
-                  className="text-4xl xl:text-6xl font-extrabold"
+                  className="text-3xl sm:text-4xl lg:text-5xl xl:text-6xl font-extrabold"
                 />
                 <p
                   className={`${
